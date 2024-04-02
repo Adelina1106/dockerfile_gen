4af84bb81a8348152a4bb0b99231dcb56fca0c44
Adelina1106 <adelina.rizea1@yahoo.com>
2024-04-03 01:31:26 +0300

    Lanfing page

--- core/settings.py
+++ core/settings.py
@@ -157,7 +157,7 @@ STATIC_URL = '/home/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'home/static'),
 )
 
 #if not DEBUG:
